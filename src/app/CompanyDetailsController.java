7a64e90bf348ab2ecb2042190ea5fbd147937583
Martin Dekov <mvdekov@gmail.com>
2020-01-13 23:54:16 +0200

    Pass TransportCompany object to child view
    Adding abitlity to init the TransportComapny object
    to the CompanyDetailsView using CompanyDetailsController
    and adding Label on top of the view to show the name
    of the selected company. Also increased window size
    
    Signed-off-by: Martin Dekov <mvdekov@gmail.com>

--- src/app/CompanyDetailsController.java
+++ src/app/CompanyDetailsController.java
@@ -1,19 +1,26 @@
 package app;
 
+import app.company.TransportCompany;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CompanyDetailsController implements Initializable {
+
+    private TransportCompany transportCompany;
+
+    @FXML
+    private Label companyName;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -24,9 +31,13 @@ public class CompanyDetailsController implements Initializable {
         Parent mainAppView = FXMLLoader.load(getClass().getResource("MainAppView.fxml"));
         Scene mainAppViewScene = new Scene(mainAppView);
         Stage mainAppWindow = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-
         mainAppWindow.setScene(mainAppViewScene);
 
         mainAppWindow.show();
+    }
+
+    public void initData(TransportCompany transportCompany){
+        this.transportCompany = transportCompany;
+        this.companyName.setText(this.transportCompany.getCompanyName());
     }
 }
